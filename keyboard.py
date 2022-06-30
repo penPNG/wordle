f219624b1446caae66aff49b7b4dbb8ff7d1571a
penPNG <graeme.roach01@yahoo.com>
2022-06-29 22:55:58 -0400

    add keyboard template

---
+++ keyboard.py
@@ -0,0 +1,14 @@
+import pygame, pygame.freetype
+BLACK = ("#000000")
+
+class Kyeboard(pygame.sprite.Sprite):
+    def __init__(self, color, width, height, letter, state):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height], 2)
+
+        self.rect = self.image.get_rect()
